7b8e5cc8392a5169095b3e6eb6abf2424c8bc765
arkoaikins <arkoaikins@gmail.com>
2024-01-04 23:47:56 +0000

    implement class airport

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,10 +1,10 @@
-class Airport {
+export default class Airport {
   constructor(name, code) {
     this._name = name;
     this._code = code;
   }
 
-  toString() {
-    return (`[object ${this._code}]`);
+  get [Symbol.toStringTag]() {
+    return this._code;
   }
 }
