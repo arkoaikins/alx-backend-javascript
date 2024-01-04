f6a32ac9a62cf9ef63642f9a60b70eddab177ffc
arkoaikins <arkoaikins@gmail.com>
2024-01-04 18:03:15 +0000

    return a promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const promises = [uploadPhoto(), createUser()];
-  Promise.all(promises)
-    .then((results) => {
-      console.log(results[0].body, results[1].firstName, results[1].lastName);
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+    .catch(() => console.log('Signup system offline'));
 }
