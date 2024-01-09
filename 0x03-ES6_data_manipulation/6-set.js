9bc056c7c9c0ddfa3e4e1e491396fa9b9be8c27e
arkoaikins <arkoaikins@gmail.com>
2024-01-09 20:11:47 +0000

    create a function that returns a Set from an array

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,7 @@
+/* Function that returns a set from an array
+ * it accepts an argument(Array of an kind of element
+ */
+
+export default function setFromArray(anyArray) {
+  return new Set([...anyArray]);
+}
