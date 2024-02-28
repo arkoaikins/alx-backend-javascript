951b30acc1e19b8bcf83c0b9963f2c9e12d7b2c7
arkoaikins <arkoaikins@gmail.com>
2024-02-28 11:59:15 -0500

    [task0]: create a function named displayMessage that prints in STDOUT the string argument

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,4 +1,10 @@
-/*  
+/*
 function named displayMessage that prints in
- STDOUT the string argument.
+STDOUT the string argument.
 */
+
+function displayMessage(string) {
+  console.log(string);
+}
+
+module.exports = displayMessage;
