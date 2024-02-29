991adcee7fddab5e59b6b4d3453c007d34d7d650
arkoaikins <arkoaikins@gmail.com>
2024-02-29 12:27:31 -0500

    [task5]:create a small HTTP server using the http module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,30 +1,30 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-function handleRequest(req, res) {
+const PORT = 1245;
+const DATABASE_PATH = process.argv[2];
+
+const app = http.createServer(async (req, res) => {
   if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    const filePath = process.argv[2]; // Access file path from arguments
-
-    countStudents(filePath)
-      .then((data) => {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(`This is the list of our students\n${data}`);
-      })
-      .catch(() => {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('This is the list of our students');
-      });
+    try {
+      const data = await countStudents(DATABASE_PATH);
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end(`This is the list of our students\n${data}`);
+    } catch (error) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('This is the list of our students');
+    }
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not found');
+    res.end('Not Found');
   }
-}
+});
 
-const app = http.createServer(handleRequest);
-
-app.listen(1245, () => console.log('Server listening on 1245'));
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
 
 module.exports = app;
