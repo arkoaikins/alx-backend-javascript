51a6e861bc72edbdc230fa704fc09ce3e877c4d9
arkoaikins <arkoaikins@gmail.com>
2024-02-29 12:29:22 -0500

    [task5]:create a small HTTP server using the http module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,30 +1,21 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const PORT = 1245;
-const DATABASE_PATH = process.argv[2];
-
-const app = http.createServer(async (req, res) => {
+const path = process.argv[2];
+const app = http.createServer((req, res) => {
   if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
-    try {
-      const data = await countStudents(DATABASE_PATH);
+  }
+  if (req.url === '/students') {
+    countStudents(path).then((data) => {
       res.writeHead(200, { 'Content-Type': 'text/plain' });
       res.end(`This is the list of our students\n${data}`);
-    } catch (error) {
+    }).catch(() => {
       res.writeHead(404, { 'Content-Type': 'text/plain' });
       res.end('This is the list of our students');
-    }
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found');
+    });
   }
 });
-
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
-
+app.listen(1245, () => console.log('Server listening on 1245'));
 module.exports = app;
