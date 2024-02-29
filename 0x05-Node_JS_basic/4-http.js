8612b8418294b60a15c654a9db7ee7a5f4e9cb48
arkoaikins <arkoaikins@gmail.com>
2024-02-28 20:39:00 -0500

    [task4]:  Create a small HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,13 +1,10 @@
 const http = require('http');
 
-function startServer() {
-  const app = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Holberton School!');
-  });
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
+});
 
-  app.listen(1245, () => console.log('Server listening on 1245'));
-}
+app.listen(1245, () => console.log('Server listening on 1245'));
 
-startServer();
-module.exports = startServer;
+module.exports = app;
