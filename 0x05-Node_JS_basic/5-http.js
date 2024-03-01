b4dc386c750fa051e4090b373a67791db3e329d6
arkoaikins <arkoaikins@gmail.com>
2024-03-01 17:32:31 +0000

    [task5]:Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,21 +1,70 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs').promises;
 
-const path = process.argv[2];
-const app = http.createServer((req, res) => {
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8')
+      .then((data) => {
+        const lines = data.split('\n');
+        const hashtable = {};
+        let students = -1;
+        let result = '';
+        for (const line of lines) {
+          if (line.trim() !== '') {
+            const columns = line.split(',');
+            const field = columns[3];
+            const firstname = columns[0];
+            if (students >= 0) {
+              if (!Object.hasOwnProperty.call(hashtable, field)) {
+                hashtable[field] = [];
+              }
+              hashtable[field] = [...hashtable[field], firstname];
+            }
+            students += 1;
+          }
+        }
+        result += `Number of students: ${students}\n`;
+        for (const key in hashtable) {
+          if (Object.hasOwnProperty.call(hashtable, key)) {
+            result += `Number of students in ${key}: ${hashtable[key].length}. List: ${hashtable[key].join(', ')}\n`;
+          }
+        }
+        resolve(result);
+      })
+      .catch(() => {
+        reject(new Error('Cannot load the database'));
+      });
+  });
+}
+
+const port = 1245;
+const host = '127.0.0.1';
+
+const handleRequest = (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
   if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    countStudents(process.argv[2])
+      .then((result) => {
+        res.end(result);
+      })
+      .catch(() => {
+        res.end('Cannot load the database');
+      });
+  } else {
+    res.statusCode = 404;
+    res.end('Not found');
   }
-  if (req.url === '/students') {
-    countStudents(path).then((data) => {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end(`This is the list of our students\n${data}`);
-    }).catch(() => {
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.end('This is the list of our students');
-    });
-  }
+};
+
+const server = http.createServer(handleRequest);
+
+server.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}/`);
 });
-app.listen(1245, () => console.log('Server listening on 1245'));
-module.exports = app;
+
+module.exports = server;
