3f77291fd9eb3d8c9b92a46f85c61397f49c99f3
arkoaikins <arkoaikins@gmail.com>
2024-03-03 16:32:46 +0000

    [task0]:Create a function named calculateNumber. It should accepts two arguments (number) a and b

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,9 @@
+/* Function should accepts two arguments (number) a and b
+ * function should round a and b and return the sum of it
+*/
+
+function calculateNumber(a, b) {
+  return Math.round(a) + Math.round(b);
+}
+
+module.exports = calculateNumber;
