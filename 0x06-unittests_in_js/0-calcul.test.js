c7a8ec47eac97ca616b9cb54cdd52650e2712396
arkoaikins <arkoaikins@gmail.com>
2024-03-03 16:40:25 +0000

    [task 0]:file that contains test cases of this function

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -13,7 +13,11 @@ describe('calculateNumber', () => {
   it('should return 2 when inputs are 0.7 and 0.6', () => {
     assert.strictEqual(calculateNumber(0.7, 0.6), 2);
   });
- it('should return NaN when passed an actual character', () => {
+  it('should return NaN when passed an actual character', () => {
    assert.strictEqual(calculateNumber('a', '3.9'), NaN)
   });
+  it('should return -4 when inputs are -3 and -2.8', () => {
+    assert.strictEqual(calculateNumber(-3, -2.8), -6);
+  });
+
 });
