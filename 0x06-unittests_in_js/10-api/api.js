7a1c1d5601eb359c2c11dbd90ac86862d72268dc
arkoaikins <arkoaikins@gmail.com>
2024-03-05 03:17:49 +0000

    [task 10]:Add an endpoint GET /available_payments that returns an object

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,14 +1,33 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
 
 app.get('/', (_req, res) => {
-    res.status(200).send('Welcome to the payment system')
-})
+  res.status(200).send('Welcome to the payment system');
+});
+
 app.get(/^\/cart\/(\d+)$/, (req, res) => {
-    const id = req.params[0]
-    res.status(200).send(`Payment methods for cart ${id}`)
-})
+  const id = req.params[0];
+  res.status(200).send(`Payment methods for cart ${id}`);
+});
+
+app.get('/available_payments', (_req, res) => {
+  const paymentMethods = {
+    credit_cards: true,
+    paypal: false
+  };
+  res.status(200).json({ payment_methods: paymentMethods });
+});
+
+app.post('/login', (req, res) => {
+  const { userName } = req.body;
+  res.status(200).send(`Welcome, ${userName}`);
+});
+
 app.listen(7865, () => {
-    console.log('API available on localhost port 7865')
-})
-module.exports = app
+  console.log('API available on localhost port 7865');
+});
+
+module.exports = app;
